a79a21fe7595dfb0eb15b66f6fa74ff7eccc922f
Victor-Matheus <victormatheusct@gmail.com>
2021-03-30 01:46:46 -0300

    fix

--- bin/server.js
+++ bin/server.js
@@ -49,7 +49,7 @@ const onListening = () => {
         `port ${address.port}`;
     debug(`Listening on ${bind}`);
 }
-
+app.use(express.static(__dirname + '/Back-end/bin'));
 server.listen(port, () => console.log(`API rodando na porta ${port}`));
 server.on("error", onError);
 server.on("listening", onListening);
